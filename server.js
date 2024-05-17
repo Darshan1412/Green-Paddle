ebfcfa2d2ee3fb9718bb6d9df1f5571299e9a408
darshan <darshanjain768@gmail.com>
2024-05-17 11:00:45 +0530

    trying to fix the issue of razorpay

--- server.js
+++ server.js
@@ -9,17 +9,17 @@ const stripe = require('stripe');
 // mongoose.connect('mongodb+srv://darshan:darshanjain1412@greenpaddle.dsqvgnm.mongodb.net/test', {useNewUrlParser:true});
 const env = require('dotenv').config();
 var ejs = require('ejs');
-const Razorpay = require('razorpay');
+// const Razorpay = require('razorpay');
 const router = express.Router();
 const routes = require('./routes')
 const authroute = require('./routes/index') 
 // const serverless = require("serverless-http");
 const port = process.env.PORT || 80
 
-const instance = new Razorpay({
-    key_id: process.env.RAZORPAY_KEY_ID,
-    key_secret: process.env.RAZORPAY_KEY_SECRET,
-  });
+// const instance = new Razorpay({
+//     key_id: process.env.RAZORPAY_KEY_ID,
+//     key_secret: process.env.RAZORPAY_KEY_SECRET,
+//   });
   
 
 // app.use(cors());
